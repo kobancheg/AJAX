e6d390534cf9aa194f94fc165afc04211a24b211
Aleksey Kozhukhov <aleks.kozhukhov@MacBook-Air.local>
2020-04-20 16:27:28 +0300

    added rendering forms

--- task.js
+++ task.js
@@ -14,21 +14,78 @@ function getUsers(cb) {
   xhr.send();
 }
 
-getUsers((response) => { 
+getUsers((response) => {
   renderUsers(response);
   userDetails(response);
- });
+});
 
 const container = document.querySelector(".container");
 const globalBody = document.querySelector("body");
 
+function createForms() {
+  const formsFragment = document.createDocumentFragment();
+  const form = document.createElement("form");
+  const row = document.createElement("div");
+  row.classList.add("row");
+  const colName = document.createElement("div");
+  colName.classList.add("col");
+  const colUserName = document.createElement("div");
+  colUserName.classList.add("col");
+  const colEmail = document.createElement("div");
+  colEmail.classList.add("col");
+  const colPhone = document.createElement("div");
+  colPhone.classList.add("col");
+  const colWebsite = document.createElement("div");
+  colWebsite.classList.add("col");
+  const inputName = document.createElement("input");
+  inputName.classList.add("form-control");
+  inputName.setAttribute("type", "text");
+  inputName.setAttribute("placeholder", "name");
+  const inputUserName = document.createElement("input");
+  inputUserName.classList.add("form-control");
+  inputUserName.setAttribute("type", "text");
+  inputUserName.setAttribute("placeholder", "username");
+  const inputEmail = document.createElement("input");
+  inputEmail.classList.add("form-control");
+  inputEmail.setAttribute("type", "text");
+  inputEmail.setAttribute("placeholder", "email");
+  const inputPhone = document.createElement("input");
+  inputPhone.classList.add("form-control");
+  inputPhone.setAttribute("type", "text");
+  inputPhone.setAttribute("placeholder", "phone");
+  const inputWebsite = document.createElement("input");
+  inputWebsite.classList.add("form-control");
+  inputWebsite.setAttribute("type", "text");
+  inputWebsite.setAttribute("placeholder", "website");
+  const button = document.createElement("button");
+  button.classList.add("btn", "btn-info");
+  button.setAttribute("type", "submit");
+  button.textContent = "add user";
+
+  colName.appendChild(inputName);
+  form.appendChild(colName);
+  colUserName.appendChild(inputUserName);
+  form.appendChild(colUserName);
+  colEmail.appendChild(inputEmail);
+  form.appendChild(colEmail);
+  colPhone.appendChild(inputPhone);
+  form.appendChild(colPhone);
+  colWebsite.appendChild(inputWebsite);
+  form.appendChild(colWebsite);
+  form.appendChild(button);
+  formsFragment.appendChild(form);
+  container.appendChild(formsFragment);
+}
+
+createForms();
+
 function userList(user) {
   const li = document.createElement("li");
   li.id = user.id;
-    li.classList.add("list-group-item");
-    li.textContent = user.name;
-    li.setAttribute("data-toggle", "modal");
-    li.dataset.target = "#modal";
+  li.classList.add("list-group-item");
+  li.textContent = user.name;
+  li.setAttribute("data-toggle", "modal");
+  li.dataset.target = "#modal";
   return li;
 }
 
@@ -38,7 +95,7 @@ function renderUsers(response) {
   card.classList.add("card");
   const ul = document.createElement("ul");
   ul.classList.add("list-group", "list-list-group-flush");
-  
+
   response.forEach((user) => {
     const li = userList(user);
     ul.appendChild(li);
@@ -88,33 +145,35 @@ function userDetails(response) {
   catchPhrases.classList.add("list-group-item");
   let bss = document.createElement("li");
   bss.classList.add("list-group-item");
-  
+
   const userItem = ({ target }) => {
-    const user = response.find((element) => element.id === +target.id);
-    const {
-      name,
-      username,
-      email,
-      address: { street, suite, city, zipcode, geo: { lat, lng } = {} } = {},
-      phone,
-      website,
-      company: { name: company, catchPhrase, bs } = {},
-    } = ({} = user);
-    
-    names.textContent = "name: " + name;
-    usernames.textContent = "username: " + username;
-    emails.textContent = "email: " + email;
-    streets.textContent = "street: " + street;
-    suites.textContent = "suite: " + suite;
-    citys.textContent = "city: " + city;
-    zipcodes.textContent = "zipcode: " + zipcode;
-    lats.textContent = "lat: " + lat;
-    lngs.textContent = "lng: " + lng;
-    phones.textContent = "phone: " + phone;
-    websites.textContent = "website: " + website;
-    companys.textContent = "company: " + company;
-    catchPhrases.textContent = "catchPhrase: " + catchPhrase;
-    bss.textContent = "bs: " + bs;
+    if (target.tagName === "LI") {
+      const user = response.find((element) => element.id === +target.id);
+      const {
+        name,
+        username,
+        email,
+        address: { street, suite, city, zipcode, geo: { lat, lng } = {} } = {},
+        phone,
+        website,
+        company: { name: company, catchPhrase, bs } = {},
+      } = ({} = user);
+
+      names.textContent = "name: " + name;
+      usernames.textContent = "username: " + username;
+      emails.textContent = "email: " + email;
+      streets.textContent = "street: " + street;
+      suites.textContent = "suite: " + suite;
+      citys.textContent = "city: " + city;
+      zipcodes.textContent = "zipcode: " + zipcode;
+      lats.textContent = "lat: " + lat;
+      lngs.textContent = "lng: " + lng;
+      phones.textContent = "phone: " + phone;
+      websites.textContent = "website: " + website;
+      companys.textContent = "company: " + company;
+      catchPhrases.textContent = "catchPhrase: " + catchPhrase;
+      bss.textContent = "bs: " + bs;
+    }
   };
 
   ul.appendChild(names);
@@ -136,6 +195,6 @@ function userDetails(response) {
   modalFade.appendChild(dialog);
   createModal.appendChild(modalFade);
   globalBody.insertBefore(createModal, container);
-  
+
   container.addEventListener("click", userItem);
 }
